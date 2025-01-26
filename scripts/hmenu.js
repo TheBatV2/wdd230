8ba0f00c750fdebee8a8ebb31eeb123ef3c4d781
Spencer Barbre <spencerbarbre@gmail.com>
2025-01-25 20:29:57 -0700

    added chamber plan

--- scripts/hmenu.js
+++ scripts/hmenu.js
@@ -1,7 +1,13 @@
+
+function hmenu (){
 const hamButton = document.querySelector('#menu');
 const mainnav = document.querySelector('.navbar');
 
 hamButton.addEventListener('click', () => {
-    mainnav.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
+    mainnav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});}
+
+document.addEventListener('DOMContentLoaded', hmenu
+    
+  );
