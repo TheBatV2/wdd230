e78d2b139aa30e493092580b65218037a222354f
Spencer Barbre <spencerbarbre@gmail.com>
2025-02-06 23:43:25 -0700

    testing ul instead of list

--- lesson05/scripts/index.js
+++ lesson05/scripts/index.js
@@ -1,7 +1,7 @@
 function DOMLoad (){
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('#list');
+const list = document.querySelector('ul');
 
 button.addEventListener('click', function() {if (input.value != 'error' ){const li = document.createElement('li');  const deleteButton = document.createElement('button'); li.textContent = input.value; deleteButton.textContent = '❌'; li.append(deleteButton); list.append(li);
     deleteButton.addEventListener('click', function () {list.removeChild(li); input.focus(); }); input.focus(); input.value = '';}});
