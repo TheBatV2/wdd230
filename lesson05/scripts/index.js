8ac029261826728ea032fe596c3220ec2fac5f44
Spencer Barbre <spencerbarbre@gmail.com>
2025-02-06 23:44:14 -0700

    removed error

--- lesson05/scripts/index.js
+++ lesson05/scripts/index.js
@@ -3,7 +3,7 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('ul');
 
-button.addEventListener('click', function() {if (input.value != 'error' ){const li = document.createElement('li');  const deleteButton = document.createElement('button'); li.textContent = input.value; deleteButton.textContent = '❌'; li.append(deleteButton); list.append(li);
+button.addEventListener('click', function() {if (input.value != '' ){const li = document.createElement('li');  const deleteButton = document.createElement('button'); li.textContent = input.value; deleteButton.textContent = '❌'; li.append(deleteButton); list.append(li);
     deleteButton.addEventListener('click', function () {list.removeChild(li); input.focus(); }); input.focus(); input.value = '';}});
 }
 document.addEventListener('DOMContentLoaded', DOMLoad
