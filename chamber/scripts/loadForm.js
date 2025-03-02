65d335b3ce7f3f75907d09e8cf67948f8e3e71cf
Spencer Barbre <spencerbarbre@gmail.com>
2025-03-01 22:27:52 -0700

    added join and surrounding pages

---
+++ chamber/scripts/loadForm.js
@@ -0,0 +1,5 @@
+
+    document.querySelector("form").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent default form submission
+        window.location.href = "thankyou.html"; // Redirect to thankyou.html
+    });
